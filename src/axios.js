4d908438e2e1d65137bc6cf64a255082414b1fd8
laoliya <936366286@qq.com>
2019-06-18 19:11:05 +0800

    修改

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ import axios from "axios";
 //设置跨域请求保存session
 axios.defaults.withCredentials=true;
 //设置请求服务器基础路径
-axios.defaults.baseURL="http://127.0.0.1:3000/";
+axios.defaults.baseURL="47.100.62.164:3000/";
 //将axio注册vue实例
 Vue.prototype.axios=axios;
 //在main.js引入axios.js
